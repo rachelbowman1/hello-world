3e3753a14a4e4d614b061c69bb1752e626a56559
Rachel Bowman <rachel.bowman1@pcc.edu>
2016-10-22 11:49:18 -0700

    added main method that prints hello world

--- src/pcc/cis/helloworld/HelloWorld.java
+++ src/pcc/cis/helloworld/HelloWorld.java
@@ -3,5 +3,10 @@ package pcc.cis.helloworld;
 /**
  * Created by Rachel on 10/22/16.
  */
-public class HelloWorld {
+public class HelloWorld
+{
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
+    }
+
 }
